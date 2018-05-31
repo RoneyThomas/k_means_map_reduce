0e73295ae508d60d482ddea7752c1a815ae3877a
Roney Thomas <roneythomas6@gmail.com>
2018-05-31 05:10:32 -0400

    Better naming

--- pack/kmeans.py
+++ pack/kmeans.py
@@ -9,7 +9,7 @@ class KMeans:
     def __init__(self, file):
         self.file_name = file
 
-    # Euclidean Distance Caculator
+    # Calculates Ecludian Distance
     def dist(self, a, b, ax=1):
         return np.linalg.norm(a - b, axis=ax)
 
@@ -19,36 +19,35 @@ class KMeans:
         # Printing the dataframe
         data.head()
         # Getting the values of height and weight in respective list
-        f1 = data['height'].values
-        f2 = data['weight'].values
+        height = data['height'].values
+        weight = data['weight'].values
         # Creating numpy array from the list
-        X = np.array(list(zip(f1, f2)))
+        X = np.array(list(zip(height, weight)))
         return X
-        # plt.scatter(f1, f2, c='black', s=7)
+        # plt.scatter(height, weight, c='black', s=7)
 
     def reduce(self, X):
         # Number of clusters
         k = 3
         # Read starting centroid values
-        startingCentroids = pd.read_csv('centroids.csv')
+        starting_centroids = pd.read_csv('centroids.csv')
         # X coordinates from centroids.csv
-        C_x = startingCentroids.iloc[:, 0]
+        C_x = starting_centroids.iloc[:, 0]
         # Y coordinates from centroids.csv
-        C_y = startingCentroids.iloc[:, 1]
+        C_y = starting_centroids.iloc[:, 1]
         C = np.array(list(zip(C_x, C_y)), dtype=np.float32)
         print("Initial Centroids")
         print(C)
-        print(startingCentroids['height'].values.tolist())
         # Plotting along with the Centroids
         # plt.scatter(f1, f2, c='#050505', s=7)
         # plt.scatter(C_x, C_y, marker='*', s=200, c='g')
         # plt.show()
         # To store the value of centroids when it updates
-        C_old = np.zeros(C.shape)
+        c_old = np.zeros(C.shape)
         # Cluster Lables(0, 1, 2)
         clusters = np.zeros(len(X))
         # Error func. - Distance between new centroids and old centroids
-        error = self.dist(C, C_old, None)
+        error = self.dist(C, c_old, None)
         # Loop will run till the error becomes zero
         while error != 0:
             # Assigning each value to its closest cluster
@@ -57,14 +56,13 @@ class KMeans:
                 cluster = np.argmin(distances)
                 clusters[i] = cluster
             # Storing the old centroid values
-            C_old = deepcopy(C)
+            c_old = deepcopy(C)
             # Finding the new centroids by taking the average value
             for i in range(k):
                 points = [X[j] for j in range(len(X)) if clusters[j] == i]
                 C[i] = np.mean(points, axis=0)
             # Euclidean Distance Caculator
-            error = self.dist(C, C_old, None)
-            print(error)
+            error = self.dist(C, c_old, None)
 
         return C, clusters, k
 
