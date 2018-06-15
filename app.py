36b94d980ad17e6dece8eaef728403be48aacc07
Roney Thomas <roneythomas6@gmail.com>
2018-06-15 02:59:16 -0400

    Uses proper way of string splitting rather than hard coding.

--- app.py
+++ app.py
@@ -56,4 +56,5 @@ def results(file_name):
     csv_data = [row for row in rdr]
     with open(f'static/graphs/{file_name}.txt') as f:
         info = [x.strip() for x in f.readlines()]
-    return render_template('results.html', filename=file_name+".png", data=csv_data, highest=info)
+    count = info[1].split(',');
+    return render_template('results.html', filename=file_name+".png", data=csv_data, highest=info, count=count)
